c3af3e0f393a882d79cf8deaffe8c5305880548f
chetantalele <chetantalele06@gmail.com>
2026-01-17 21:51:45 +0530

    update

--- terraform/providers-k8s.tf
+++ terraform/providers-k8s.tf
@@ -6,6 +6,3 @@ data "aws_eks_cluster_auth" "cluster" {
   name = aws_eks_cluster.cluster.name
 }
 
-data "aws_iam_openid_connect_provider" "eks" {
-  url = aws_eks_cluster.cluster.identity[0].oidc[0].issuer
-}
