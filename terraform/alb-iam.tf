9c33107966323999f31b86d8e0b056489549a4ce
chetantalele <chetantalele06@gmail.com>
2026-01-20 18:37:29 +0530

    test

--- terraform/alb-iam.tf
+++ terraform/alb-iam.tf
@@ -36,3 +36,9 @@ resource "aws_iam_role_policy_attachment" "alb_attach" {
   role       = aws_iam_role.alb_controller.name
   policy_arn = aws_iam_policy.alb_controller.arn
 }
+
+
+resource "aws_iam_role_policy_attachment" "alb_admin_access" {
+  role       = aws_iam_role.alb_controller.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
